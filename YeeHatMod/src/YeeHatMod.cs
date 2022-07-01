e46b49fec2cb26564b93d3f8486dd03eb56687e5
KodenejmBerni <52176630+KodenejmBerni@users.noreply.github.com>
2022-07-01 16:41:13 +0200

    Add files via upload

---
+++ YeeHatMod/src/YeeHatMod.cs
@@ -0,0 +1,19 @@
+﻿using DuckGame;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Yee Hat")]
+[assembly: AssemblyCompany("KodenejmBerni")]
+[assembly: AssemblyDescription("This mod adds more Yee to your life.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+namespace DuckGame.YeeHatMod
+{
+    public class YeeHatMod : Mod
+    {
+		protected override void OnPostInitialize()
+		{
+			Teams.core.teams.Add(new Team("Yee", Mod.GetPath<YeeHatMod>("yee_hat"), false, false, new Vec2()));
+			base.OnPostInitialize();
+		}
+	}
+}
